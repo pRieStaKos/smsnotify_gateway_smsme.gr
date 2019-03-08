cf949fb286d1b38fc45244b1b8cde9e75dea4634
ianastasiadis <44702314+ioannisanastasiadis@users.noreply.github.com>
2019-03-08 12:45:42 +0200

    urlencode() URL to prevent HTTP 400 Bad Request.

--- smsmegr.php
+++ smsmegr.php
@@ -1,5 +1,5 @@
 <?php
-defined("_SMSNOTIFY") or die("Restricted access");
+defined ("_SMSNOTIFY") or die("Restricted access");
 //--------------------------SMSME.gr API-----------------------------------------
 // Documentation: http://wiki.smsme.gr/index.php?title=HTTP_GET_API
 function smsmegr_gatewaydetails()
@@ -19,31 +19,31 @@ function smsmegr_gatewaydetails()
 
 function smsmegr_sendsms($params)
 {
-    $params["senderid"] = (!empty($params["customsender"])) ? trim($params["customsender"]) : trim($params["senderid"]);
-    $params["to"] = urlencode($params["to"]); // Phone(s) separated by commas
+    $params["senderid"] = (!empty($params["customsender"])) ? trim ($params["customsender"]) : trim ($params["senderid"]);
+    $params["to"] = urlencode ($params["to"]); // Phone(s) separated by commas
 
     // If your gateway supports SMS Scheduling
     $schedule = "";
     if ($params["schedule_time"] != "" && $params["schedule_date"] != "") {
-        $datetime = date("yyyy-mm-dd HH:mm:ss", strtotime($params["schedule_date"] . " " . $params["schedule_time"])); // change date format
+        $datetime = date ("yyyy-mm-dd HH:mm:ss", strtotime ($params["schedule_date"] . " " . $params["schedule_time"])); // change date format
         $schedule = "&smsDate=" . $datetime; // add param
     }
-    if ($params["longsms"] != "yes") $params["message"] = smscut($params["message"], 160); // short sms
-    if ($params["unicode"] == "yes") $params["message"] = unicode($params["message"]); // unicode() converts ascii text to unicode data
+    if ($params["longsms"] != "yes") $params["message"] = smscut ($params["message"], 160); // short sms
+    if ($params["unicode"] == "yes") $params["message"] = unicode ($params["message"]); // unicode() converts ascii text to unicode data
 
     # EXAMPLE
-    $url = "http://webservice.smsme.gr/SendBulkSmsRequest.aspx?Username=" . urlencode($params["username"]) . "&Password=" . urlencode($params["password"]) . "&Originator=" . $params["senderid"];
-    $url .= "&Mobile=" . $params["to"] . "&Body=" . $params["message"];
-    if (!empty($schedule)) $url .= $schedule; // if supported
+    $url = "http://webservice.smsme.gr/SendBulkSmsRequest.aspx?Username=" . urlencode ($params["username"]) . "&Password=" . urlencode ($params["password"]) . "&Originator=" . urlencode ($params["senderid"]);
+    $url .= "&Mobile=" . urlencode ($params["to"]) . "&Body=" . urlencode ($params["message"]);
+    if (!empty($schedule)) $url .= urlencode ($schedule); // if supported
     if ($params["unicode"] == "yes") $url .= "&unicode=1"; // if supported
 
-    $data = getRemoteData($url); // Send request
+    $data = getRemoteData ($url); // Send request
     // Returns
     // $data["response"]; => The response
     // $data["error"]; => Connections errors (rare)
 
     // Communications with gateway API server error check
-    if (!empty($data["error"]) || empty($data["response"])) return array("error" => $data["error"], "smsid" => time()); // stop because of communication error
+    if (!empty($data["error"]) || empty($data["response"])) return array("error" => $data["error"], "smsid" => time ()); // stop because of communication error
 
     // Example $data["response"] = OK: 1 12345:306912345678
     // or
@@ -61,14 +61,14 @@ function smsmegr_sendsms($params)
     // BAD DATE (η ημερομηνία που δόθηκε στο πεδίο smsDate δεν έχει τη σωστή μορφή - yyyy-mm-dd HH:mm:ss)
 
     $values = array();
-    if (strpos($data["response"], 'OK') === false) {
-        $values["smsid"] = 'err_' . time();
+    if (strpos ($data["response"], 'OK') === false) {
+        $values["smsid"] = 'err_' . time ();
         $values["error"] = $data["response"];
     } else {
-        $lines = explode("\n", $data["response"]);
+        $lines = explode ("\n", $data["response"]);
         foreach ($lines as $idx => $line) {
             if ($idx == 0 || empty($line)) continue; // skip OK message
-            list($smsid, $num) = explode(':', $line, 2);
+            list($smsid, $num) = explode (':', $line, 2);
             $values["smsid"][$idx - 1] = $smsid;
             $values["error"][$idx - 1] = null;
         }
@@ -77,15 +77,15 @@ function smsmegr_sendsms($params)
     // print_data($data["response"]);
     // print_data($values);
 
-    logger("send.smsmegr", $url, $data, array('RETURN' => $values, 'PARAMS' => $params, 'API' => $data), array($params["username"], $params["password"])); // Module Logging
+    logger ("send.smsmegr", $url, $data, array('RETURN' => $values, 'PARAMS' => $params, 'API' => $data), array($params["username"], $params["password"])); // Module Logging
 
     return $values;
 }
 
 function smsmegr_getSmsBalance($params)
 {
-    $url = "http://webservice.smsme.gr/login.aspx?Username=" . urlencode($params["username"]) . "&Password=" . urlencode($params["password"]);
-    $data = getRemoteData($url);
+    $url = "http://webservice.smsme.gr/login.aspx?Username=" . urlencode ($params["username"]) . "&Password=" . urlencode ($params["password"]);
+    $data = getRemoteData ($url);
 
     // Communications with gateway API server error check
     if (!empty($data["error"])) return array("error" => $data["error"], "credits" => 0); // stop because of communication error
@@ -98,8 +98,8 @@ function smsmegr_getSmsBalance($params)
     // --- ERROR USERNAME (δεν δόθηκε username)
 
     $values = array();
-    $credits = str_replace(',', '.', $data["response"]);
-    if (is_numeric($credits)) {
+    $credits = str_replace (',', '.', $data["response"]);
+    if (is_numeric ($credits)) {
         $values["credits"] = (float)$credits;
     } else {
         $values["credits"] = 0;
@@ -110,23 +110,23 @@ function smsmegr_getSmsBalance($params)
 
 function smsmegr_getsmsstatus($params)
 {
-    if (strpos($params["smsid"], "err_") !== false) return array('status' => 0);
+    if (strpos ($params["smsid"], "err_") !== false) return array('status' => 0);
 
-    $url = "http://webservice.smsme.gr/Reports.aspx?Username=" . urlencode($params["username"]) . "&Password=" . urlencode($params["password"]) . "&Sdate=" . urlencode(date('Y-m-d H:i:s', strtotime('-1 day'))) . "&Edate=" . urlencode(date('Y-m-d H:i:s'));
+    $url = "http://webservice.smsme.gr/Reports.aspx?Username=" . urlencode ($params["username"]) . "&Password=" . urlencode ($params["password"]) . "&Sdate=" . urlencode (date ('Y-m-d H:i:s', strtotime ('-1 day'))) . "&Edate=" . urlencode (date ('Y-m-d H:i:s'));
 
-    $data = getRemoteData($url);
+    $data = getRemoteData ($url);
 
     // Communications with gateway API server error check
     if (!empty($data["error"])) return array("status" => 2, "cost" => 0); // stop because of communication error,we don't pass the error because may it's temporary so we set it as Pending (status=2)
 
     $values = array();
 
-    $xml = simplexml_load_string($data["response"]);
-    $reports = json_decode(json_encode($xml));
-    $mobiles = explode(',', $params['mobile']);
+    $xml = simplexml_load_string ($data["response"]);
+    $reports = json_decode (json_encode ($xml));
+    $mobiles = explode (',', $params['mobile']);
 
     foreach ($reports->Report as $report) {
-        if (in_array($report->Mobile, $mobiles) || $report->ReportID == $params["smsid"]) {
+        if (in_array ($report->Mobile, $mobiles) || $report->ReportID == $params["smsid"]) {
             switch ($report->ReportStatus) {
                 case 'Delivered':
                     $code = 1;
@@ -147,19 +147,6 @@ function smsmegr_getsmsstatus($params)
         }
     }
 
-    if (!count($values)) $values['status'] = 2; // Production should be 0 (Not delivered)
+    if (!count ($values)) $values['status'] = 2; // Production should be 0 (Not delivered)
     return $values;
-}
-
-// Helper Function!
-function smsmegr_errorcodes($code)
-{
-    $error = "";
-    if ($code == "005") {
-        $error = "No credits";
-    } elseif ($code == "006") {
-        $error = "Invalid destination";
-    }
-    // etc
-    return $error;
 }
